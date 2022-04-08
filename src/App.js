67fb747a0eb9972cbbb2aed1012458044db8f248
lutfor779 <mdlrrahman779@gmail.com>
2022-04-08 17:39:29 +0600

    initial setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Button, Input } from "antd";
+import "./App.less";
+const { Search } = Input;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Button type="primary">Button</Button>
+			<Search
+				placeholder="input search text"
+				allowClear
+				onSearch={(e) => console.log(e)}
+				enterButton
+			/>
+			<p className="bg-red-100 text-xl font-bold">
+				Hello this is from tailwindcss
+			</p>
+		</div>
+	);
 }
 
 export default App;
